0386971e568f136f3d3e3608a000b71f89c56e7d
Faizan Saiyed <faizan.saiyed@thesunflowerlab.com>
2020-10-02 07:59:30 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,68 @@
+![Liner Logo](https://liner.faizansaiyed.ml/images/liner-logo.png "Liner Logo")
+
+Welcome to Liner Pro! [Homepage](https://liner.faizansaiyed.ml)
+===================
+### Creative OnePage Responsive Bootstrap Template ###
+
+Retina ready, fully responsive, crystal clear line icons, developed on most popular - Bootstrap Framework, Liner Pro is just all you need!
+
+----------
+
+
+Features
+-------------
+
+![Liner Features](https://liner.faizansaiyed.ml/images/liner-features.png "Liner Feataures")
+
+> **Did you know?** Liner is evolving and is updated very frequently. Stay tuned!
+
+
+----------
+
+
+Why Bootstrap?
+-------------------
+
+**[Boostrap](https://getbootstrap.com/)** is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.
+
+#### <i class="icon-refresh"></i> Who is it for?
+
+Liner is built for Everyone! To get awesome websites up and running fast, with less work, digging, and headaches.
+
+#### <i class="icon-refresh"></i> Why you'll love it.
+
+Liner comes with three versions with four color combinations. Just choose your style and you are good to go! Pretty enough na?
+
+----------
+
+
+Demos
+-------------
+
+Liner comes with three versions:
+ 1. [Default Layout](https://www.faizansaiyed.ml/liner/demo/index.html)
+ 2. [Slider Layout](https://www.faizansaiyed.ml/liner/demo/index-slider.html)
+ 3. [Background Video Layout](https://www.faizansaiyed.ml/liner/demo/index-video.html)
+
+Its not just three!! Liner comes with four color presets too:
+ 1. [Vegie Green](https://www.faizansaiyed.ml/liner/demo/index-green.html)
+ 2. [Dust Yellow](https://www.faizansaiyed.ml/liner/demo/index-yellow.html)
+ 3. [Lust Red](https://www.faizansaiyed.ml/liner/demo/index-red.html)
+ 4. [Mystic Blue](https://www.faizansaiyed.ml/liner/demo/index-blue.html)
+
+Just choose your styles and start building!
+
+
+----------
+
+
+Notes:
+--------------------
+#### Special Thanks:
+ 1. [Bootstrap Framework](https://getbootstrap.com/)
+ 2. [Linea Icons](https://linea.io)
+ 3. [SweetAlert](https://sweetalert.js.org/)
+ 4. [Trianglify](https://qrohlf.com/trianglify-generator/) (For Background Images)
+ 5. [Coverr](https://coverr.co) (For Background Video)
+
+
---
+++ assets/js/backup-main.js
@@ -0,0 +1,166 @@
+// Smooth Parallax Effect
+$(document).on('scroll', function () {
+    var $movebg = $(window).scrollTop() * -0.3;
+    $('.background-image').css('background-positionY', ($movebg) + 'px');
+});
+
+//jQuery is required to run this responsive background video code
+$(function() {
+
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+});
+
+function scaleVideoContainer() {
+
+    var height = $(window).height() + 5;
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
+
+}
+
+function initBannerVideoSize(element){
+
+    $(element).each(function(){
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+}
+
+function scaleBannerVideoSize(element){
+
+    var windowWidth = $(window).width(),
+    windowHeight = $(window).height() + 5,
+    videoWidth,
+    videoHeight;
+
+    // console.log(windowHeight);
+
+    $(element).each(function(){
+        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+        $(this).width(windowWidth);
+
+        if(windowWidth < 1620){
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+            $(this).width(videoWidth).height(videoHeight);
+        }
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+    });
+}
+
+// Smooth Scroll
+$(document).on('click', '.nav-links', function(event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+// Scroll to top
+$(function($) {
+    // browser window scroll (in pixels) after which the "back to top" link is shown
+    var offset = 300,
+        //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+        offset_opacity = 1200,
+        //duration of the top scrolling animation (in ms)
+        scroll_top_duration = 700,
+        //grab the "back to top" link
+        $back_to_top = $('.cd-top');
+
+    //hide or show the "back to top" link
+    $(window).on('scroll', function() {
+        ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible'): $back_to_top.removeClass('cd-is-visible cd-fade-out');
+        if ($(this).scrollTop() > offset_opacity) {
+            $back_to_top.addClass('cd-fade-out');
+        }
+    });
+
+    //smooth scroll to top
+    $back_to_top.on('click', function(event) {
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0,
+        }, scroll_top_duration);
+    });
+
+});
+
+// Close collapse navbar in small device when any link is clicked
+$(document).on('click', '.navbar-collapse.in', function(e) {
+    if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+        $(this).collapse('hide');
+    }
+});
+
+// Stop youtube video on modal close
+
+// If YouTube Video
+$('.corporateVideo').on('hide.bs.modal', function(e) {    
+    var $if = $(e.delegateTarget).find('iframe');
+    var src = $if.attr("src");
+    $if.attr("src", '/empty.html');
+    $if.attr("src", src);
+});
+
+// If local video file
+$('.corporateVideo').on('hide.bs.modal', function(e) {    
+    $(this).find('video')[0].pause();
+});
+
+// Contact Form
+$(function ($) {
+
+    'use strict';
+
+    $('#contact').on('submit',function(e){
+
+        e.preventDefault();
+
+        $('#submit').attr('disabled', true);
+
+        var $action = $(this).prop('action');
+        var $data = $(this).serialize();
+        var $this = $(this);
+
+        $this.prevAll('.alert').remove();
+
+        $.post( $action, $data, function( data ) {
+
+            if( data.response=='error' ){
+
+                $this.before( '<div class="alert alert-danger alert-dismissable"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+data.message+'</div>' );
+                $('#submit').attr('disabled', false);
+            }
+
+            if( data.response=='success' ){
+
+                $this.before( '<div class="alert alert-success alert-dismissable"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+data.message+'</div>' );
+                $this.find('input, textarea').val('');
+                $('#submit').attr('disabled', false);
+            }
+
+        }, "json");
+
+    });
+});
